4e6287979f7d5a84816fe2644bf4c05b45449213
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-04-18 10:15:40 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,7 +5,8 @@ import os
 
 # 目标URL列表
 urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
-        'https://ip.164746.xyz'
+        'https://ip.164746.xyz',
+        'https://cfno1.pages.dev/pure'
         ]
 
 # 正则表达式用于匹配IP地址
